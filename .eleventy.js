568f836cc06fc97c0d44e357cb2aee5ef7c084ee
Brannon Crumpton <bcrumpton72@gmail.com>
2026-01-09 01:25:58 -0600

    Removed commented code no longer used

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,3 @@
-// const svgContents = require("eleventy-plugin-svg-contents");
-// const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
-
-// module.exports = function(eleventyConfig) {
-//   eleventyConfig.addPlugin(UpgradeHelper);
-//   eleventyConfig.addPassthroughCopy('./src/css/styles.css');
-//   eleventyConfig.addPassthroughCopy('./src/img');
-//   eleventyConfig.addPassthroughCopy('./src/about/images');
-//   eleventyConfig.addPlugin(svgContents);
-//   return {
-//     dir: {
-//       input: 'src',
-//       output: 'public'
-//     }
-//   }
-// }
-
 const fs = require('fs');
 const path = require('path');
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
@@ -26,12 +9,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/img');
   eleventyConfig.addPassthroughCopy('./src/about/images');
   
-  // Remove this line:
-  // eleventyConfig.addPlugin(svgContents);
-  
-  // Add this custom shortcode instead:
   eleventyConfig.addFilter("svgContents", function(filePath) {
-    // Handle both relative and absolute paths
     const cleanPath = filePath.replace(/^\.?\//, ''); // Remove leading ./ or /
     const svgPath = path.join(__dirname, 'src', cleanPath);
     
