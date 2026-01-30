3f59c6bdfca33d39efe153f952da56b3b1c1e62c
Brannon Crumpton <bcrumpton72@gmail.com>
2026-01-30 16:33:43 -0600

    Removed unused packages, updated styles and added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(UpgradeHelper);
+module.exports = function (eleventyConfig) {
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy('./src/css/styles.css');
   eleventyConfig.addPassthroughCopy('./src/img');
   eleventyConfig.addPassthroughCopy('./src/about/images');
-  
-  eleventyConfig.addFilter("svgContents", function(filePath) {
+
+  eleventyConfig.addFilter("svgContents", function (filePath) {
     const cleanPath = filePath.replace(/^\.?\//, ''); // Remove leading ./ or /
     const svgPath = path.join(__dirname, 'src', cleanPath);
-    
+
     try {
       return fs.readFileSync(svgPath, 'utf8');
     } catch (error) {
@@ -20,7 +18,7 @@ module.exports = function(eleventyConfig) {
       return '';
     }
   });
-  
+
   return {
     dir: {
       input: 'src',
